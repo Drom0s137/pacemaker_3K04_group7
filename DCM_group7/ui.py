45cb20c5b590121206c2e4531a011ed47046e1ad
Eddy <eddy.che.su@gmail.com>
2023-09-30 14:44:34 -0400

    revert

--- DCM_group7/ui.py
+++ DCM_group7/ui.py
@@ -1,64 +1,33 @@
 from tkinter import *
-import customtkinter
-import backend
-import sys
-
-global login
-
-def temp_text(e, i):
-    i.delete(0, "end")
-
-def obtain_logins(ui_username, ui_pswrd):
-    username = ui_username.get()
-    password = ui_pswrd.get()
-    if backend.log_in(username, password):
-        login = True
-    else:
-        login = False
-
-def register_user(ui_username, ui_pswrd):
-    username = ui_username.get()
-    password = ui_pswrd.get()
-    if backend.register(username, password):
-        login = 1
-    else:
-        login = 0
-
-def main():
-    welcome = Tk()
-    welcome.title("Pacemaker UI")
-    welcome.geometry("500x580")
-
-    label = Label(welcome, text="Welcome to Pacemaker Interface", font=('Arial', 14))
-    label.pack(padx=20, pady=20)
-
-    #username section
-    ui_username = Entry(welcome, bg = "white", width=20)
-    ui_username.insert(0, "Enter Username")
-    ui_username.pack(pady=2, padx=10)
-    def handler(event, i=ui_username):   
-        return temp_text(event, i)
-    ui_username.bind("<FocusIn>", handler)
-
-    #password section
-    ui_pswrd = Entry(welcome, width=20)
-    ui_pswrd.insert(0, "Enter Password")
-    ui_pswrd.pack(pady=2, padx=10)
-    def handler(event, i=ui_pswrd):   
-        return temp_text(event, i)
-    ui_pswrd.bind("<FocusIn>", handler)
-
-    login = Button(welcome, command = lambda: obtain_logins(ui_username, ui_pswrd), text="Login", font=('Arial', 12))
-    login.pack(padx=20, pady=10)
-
-    register = Button(welcome, command = lambda: register_user(ui_username, ui_pswrd), text="Register", font=('Arial', 12))
-    register.pack(padx=20, pady=10)
-
-    quit = Button(welcome, command = backend.exit_system, text="Quit", font=('Arial', 12))
-    quit.pack(padx=20, pady=10)
-
-    welcome.mainloop()
 
 
-if __name__ == "__main__":
-    main()
+welcome = Tk()
+welcome.title("Pacemaker UI")
+welcome.geometry("500x580")
+
+label = Label(welcome, text="Welcome to Pacemaker Interface", font=('Arial', 14))
+label.pack(padx=20, pady=20)
+
+username = Entry(welcome, width=20)
+username.insert(0, "Enter Username")
+username.pack(pady=2, padx=10)
+# self.username.bind("<FocusIn>", DCM.clr_text)
+
+password = Entry(welcome, width=20)
+password.insert(0, "Enter Password")
+password.pack(pady=2, padx=10)
+# password.bind("<FocusIn>", DCM.clr_text(self, self.password))
+
+login = Button(welcome, text="Login", font=('Arial', 12))
+login.pack(padx=20, pady=10)
+
+register = Button(welcome, text="Register", font=('Arial', 12))
+register.pack(padx=20, pady=10)
+
+quit = Button(welcome, text="Quit", font=('Arial', 12))
+quit.pack(padx=20, pady=10)
+
+welcome.mainloop()
+
+    # def clr_text(self):
+    #     self.delete(0, "end")
