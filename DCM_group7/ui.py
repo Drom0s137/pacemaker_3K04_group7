f8e082a01f8e8ee86988c922f0ec022efca32e40
Grigor Pahlevanyan <pahlevag@mcmaster.ca>
2023-09-30 12:58:23 -0400

    UI Upload

--- DCM_group7/ui.py
+++ DCM_group7/ui.py
@@ -1,148 +1,38 @@
-
-import matplotlib
-matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTk, NavigationToolbar2Tk
-from matplotlib.figure import Figure
-import matplotlib.animation as animation
-from matplotlib import style
-
-import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 
 
-LARGE_FONT= ("Verdana", 12)
-style.use("ggplot")
+class DCM:
 
-f = Figure(figsize=(5,5), dpi=100)
-a = f.add_subplot(111)
+    def __init__(self):
+        self.welcome = Tk()
+        self.welcome.title("Pacemaker UI")
+        self.welcome.geometry("500x580")
 
+        self.label = Label(self.welcome, text="Welcome to Pacemaker Interface", font=('Arial', 14))
+        self.label.pack(padx=20, pady=20)
 
-def animate(i):
-    pullData = open("sampleText.txt","r").read()
-    dataList = pullData.split('\n')
-    xList = []
-    yList = []
-    for eachLine in dataList:
-        if len(eachLine) > 1:
-            x, y = eachLine.split(',')
-            xList.append(int(x))
-            yList.append(int(y))
+        self.username = Entry(self.welcome, width=20)
+        self.username.insert(0, "Enter Username")
+        self.username.pack(pady=2, padx=10)
+        # self.username.bind("<FocusIn>", DCM.clr_text)
 
-    a.clear()
-    a.plot(xList, yList)
+        self.password = Entry(self.welcome, width=20)
+        self.password.insert(0, "Enter Password")
+        self.password.pack(pady=2, padx=10)
+        # self.password.bind("<FocusIn>", DCM.clr_text(self, self.password))
 
-    
-            
+        self.login = Button(self.welcome, text="Login", font=('Arial', 12))
+        self.login.pack(padx=20, pady=10)
 
-class SeaofBTCapp(tk.Tk):
+        self.register = Button(self.welcome, text="Register", font=('Arial', 12))
+        self.register.pack(padx=20, pady=10)
 
-    def __init__(self, *args, **kwargs):
-        
-        tk.Tk.__init__(self, *args, **kwargs)
+        self.quit = Button(self.welcome, text="Quit", font=('Arial', 12))
+        self.quit.pack(padx=20, pady=10)
 
-        tk.Tk.iconbitmap(self, default="clienticon.ico")
-        tk.Tk.wm_title(self, "Sea of BTC client")
-        
-        
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand = True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.welcome.mainloop()
 
-        self.frames = {}
+    # def clr_text(self):
+    #     self.delete(0, "end")
 
-        for F in (StartPage, PageOne, PageTwo, PageThree):
-
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-
-        frame = self.frames[cont]
-        frame.tkraise()
-
-        
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button = ttk.Button(self, text="Visit Page 1",
-                            command=lambda: controller.show_frame(PageOne))
-        button.pack()
-
-        button2 = ttk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-        button3 = ttk.Button(self, text="Graph Page",
-                            command=lambda: controller.show_frame(PageThree))
-        button3.pack()
-
-
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Page Two",
-                            command=lambda: controller.show_frame(PageTwo))
-        button2.pack()
-
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Page One",
-                            command=lambda: controller.show_frame(PageOne))
-        button2.pack()
-
-
-class PageThree(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-        button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        
-
-        
-
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.show()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-        toolbar = NavigationToolbar2TkAgg(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-
-
-app = SeaofBTCapp()
-ani = animation.FuncAnimation(f, animate, interval=1000)
-app.mainloop()
+DCM()
