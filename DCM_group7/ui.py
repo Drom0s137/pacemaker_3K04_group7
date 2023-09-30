4f809c25719fdfbb8a7ac47e648f6f47295a7704
Eddy <eddy.che.su@gmail.com>
2023-09-30 13:06:29 -0400

    class ui system removal

--- DCM_group7/ui.py
+++ DCM_group7/ui.py
@@ -1,38 +1,33 @@
 from tkinter import *
 
 
-class DCM:
+welcome = Tk()
+welcome.title("Pacemaker UI")
+welcome.geometry("500x580")
 
-    def __init__(self):
-        self.welcome = Tk()
-        self.welcome.title("Pacemaker UI")
-        self.welcome.geometry("500x580")
+label = Label(welcome, text="Welcome to Pacemaker Interface", font=('Arial', 14))
+label.pack(padx=20, pady=20)
 
-        self.label = Label(self.welcome, text="Welcome to Pacemaker Interface", font=('Arial', 14))
-        self.label.pack(padx=20, pady=20)
+username = Entry(welcome, width=20)
+username.insert(0, "Enter Username")
+username.pack(pady=2, padx=10)
+# self.username.bind("<FocusIn>", DCM.clr_text)
 
-        self.username = Entry(self.welcome, width=20)
-        self.username.insert(0, "Enter Username")
-        self.username.pack(pady=2, padx=10)
-        # self.username.bind("<FocusIn>", DCM.clr_text)
+password = Entry(welcome, width=20)
+password.insert(0, "Enter Password")
+password.pack(pady=2, padx=10)
+# password.bind("<FocusIn>", DCM.clr_text(self, self.password))
 
-        self.password = Entry(self.welcome, width=20)
-        self.password.insert(0, "Enter Password")
-        self.password.pack(pady=2, padx=10)
-        # self.password.bind("<FocusIn>", DCM.clr_text(self, self.password))
+login = Button(welcome, text="Login", font=('Arial', 12))
+login.pack(padx=20, pady=10)
 
-        self.login = Button(self.welcome, text="Login", font=('Arial', 12))
-        self.login.pack(padx=20, pady=10)
+register = Button(welcome, text="Register", font=('Arial', 12))
+register.pack(padx=20, pady=10)
 
-        self.register = Button(self.welcome, text="Register", font=('Arial', 12))
-        self.register.pack(padx=20, pady=10)
+quit = Button(welcome, text="Quit", font=('Arial', 12))
+quit.pack(padx=20, pady=10)
 
-        self.quit = Button(self.welcome, text="Quit", font=('Arial', 12))
-        self.quit.pack(padx=20, pady=10)
-
-        self.welcome.mainloop()
+welcome.mainloop()
 
     # def clr_text(self):
     #     self.delete(0, "end")
-
-DCM()
