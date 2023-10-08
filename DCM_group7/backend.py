2d9cad55f065bfc7436bf1d1e166e3ed8d5fb9df
Grigor Pahlevanyan <pahlevag@mcmaster.ca>
2023-10-08 16:33:05 -0400

    Added error message popup functionality. Waiting on save button error fix from backend

--- DCM_group7/backend.py
+++ DCM_group7/backend.py
@@ -2,7 +2,6 @@ import json
 import logging
 import sys
 
-
 def exit_system():
     sys.exit()
 
@@ -10,28 +9,33 @@ def log_in(username, password):
     data, usercount = extract_database()
     if username == "Enter Username" or username == "" or password == "Enter Password" or password == "":
         print("Missing information, please complete both sections before continuing")
-        return 0
+        error_msg = "Unsuccessful Login"
+        return 0, error_msg
     for user in data:
         if user["user_name"] == username and user["password"] == password:
             print("log in successful")
             return 1
     
     print("no matching data avaiable")
-    return 0
+    error_msg = "No match"
+    return 0, error_msg
     
 
 def register(username, password):
     data, usercount = extract_database()
     if usercount == 10:
         print("too many users, please remove users first")
-        return 0 
+        error_msg = "Too many users, delete users"
+        return 0, error_msg 
     if username == "Enter Username" or username == "" or password == "Enter Password" or password == "":
         print("Missing information, please complete both sections before continuing")
-        return 0
+        error_msg = "Missing Information"
+        return 0, error_msg
     for user in data:
         if user["user_name"] == username:
             print("user already exists, please log in instead")
-            return 0
+            error_msg = "User already exists"
+            return 0, error_msg
     
         
     data.append({
@@ -45,7 +49,7 @@ def register(username, password):
                             separators=(',',': '))
     
     print('Successfully registered, please try logging in')
-    return 1
+    return 1, "Success"
 
 def extract_database():
     # user JSON load in 
@@ -67,7 +71,8 @@ ARR: Atrial Refractory Period
 def verifyInput(URL, LRL, APW=None, AA=None, RS=None, AS=None, ARR=None, VPW=None, VA=None, VS=None, VRR=None):
     if URL < LRL:  #basic logic limiter since lower limit cant be higher then upper limit
         print("Lower Rate Limit Cannot Be Higher Than the Upper Rate Limit")
-        return 0 
+        error_msg = "LRL cannot be greater than URL"
+        return 0
     elif LRL < 30 or LRL > 175:
         print("LRL out of bounds")
     elif URL <50 or URL > 175:
--- DCM_group7/ui.py
+++ DCM_group7/ui.py
@@ -16,28 +16,33 @@ def switch_frame(new, old):
 def obtain_logins(ui_username, ui_pswrd):
     username = ui_username.get()
     password = ui_pswrd.get()
-    if backend.log_in(username, password):
+    if backend.log_in(username, password) == 1:
         print("chaging to mode view")
         switch_frame(modes, welcome)
+    else:
+        display_msg(backend.log_in(username, password)[1])
 
 def register_user(ui_username, ui_pswrd):
     username = ui_username.get()
     password = ui_pswrd.get()
-    backend.register(username, password)
-
-
+    temp = backend.register(username, password)
+    if temp[0] == 0:
+        display_msg(backend.register(username, password)[1])
+    else:
+        display_msg("\tSUCCESS\t")
+    
 def welcome_page(welcome):
     #welcome = Modes_page(mode)
 
     #welcome = ThemedTk(theme="arc")
 
-    label = ttk.Label(welcome, text="Welcome to Pacemaker Interface", font=('Arial', 14))
-    label.pack(padx=20, pady=20)
+    label = ttk.Label(welcome, text="\tWelcome to Pacemaker Interface", font=('Arial', 14))
+    label.grid(row=0, column=0, columnspan=3)
 
     #username section
     ui_username = Entry(welcome, bg = "white", width=20)
     ui_username.insert(0, "Enter Username")
-    ui_username.pack(pady=2, padx=10)
+    ui_username.grid(row=1,column=1, pady=5)
     def handler(event, i=ui_username):   
         return temp_text(event, i)
     ui_username.bind("<FocusIn>", handler)
@@ -45,22 +50,22 @@ def welcome_page(welcome):
     #password section
     ui_pswrd = Entry(welcome, width=20)
     ui_pswrd.insert(0, "Enter Password")
-    ui_pswrd.pack(pady=2, padx=10)
+    ui_pswrd.grid(row=2,column=1, pady=5)
     def handler(event, i=ui_pswrd):   
         return temp_text(event, i)
     ui_pswrd.bind("<FocusIn>", handler)
 
     login = ttk.Button(welcome, command = lambda: obtain_logins(ui_username, ui_pswrd), text="Login")
-    login.pack(padx=20, pady=10)
+    login.grid(row=3,column=1, pady=5)
 
     register = ttk.Button(welcome, command = lambda: register_user(ui_username, ui_pswrd), text="Register")
-    register.pack(padx=20, pady=10)
-
-    # log_out = ttk.Button(welcome, text="Log Out")
-    # log_out.pack(padx=20, pady=10)
+    register.grid(row=4,column=1, pady=5)
 
     quit = ttk.Button(welcome, command = backend.exit_system, text="Quit")
-    quit.pack(padx=20, pady=10)
+    quit.grid(row=5,column=1, pady=5)
+
+    l0 = Label(welcome, width=10, height=3) # This is blank space just to help center the layout 
+    l0.grid(column=0, row=0, rowspan=10)
 
 def Modes_page(Modes, Welcome):
 
@@ -80,6 +85,10 @@ def Modes_page(Modes, Welcome):
     VVI_btn.pack(padx=20, pady=10)
     back = ttk.Button(Modes, text="BACK", width=10, command=lambda: switch_frame(Welcome, Modes))
     back.pack(pady=10)
+
+def display_msg(msg):
+    label = ttk.Label(welcome, text=msg, foreground="red", font=('Arial', 10))
+    label.grid(row=10,column=1)
 
 if __name__ == "__main__":
     #win = Tk()
