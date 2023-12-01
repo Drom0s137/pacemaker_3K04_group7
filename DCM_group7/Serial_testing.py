9999fc89bdfe500a8732f6484bb2142566ae6ac4
Grigor Pahlevanyan <pahlevag@mcmaster.ca>
2023-11-30 20:30:31 -0500

    Update

--- DCM_group7/Serial_testing.py
+++ DCM_group7/Serial_testing.py
@@ -18,12 +18,10 @@ ser.isOpen()
 
 print ("Enter your commands below.\r\nInsert exit to leave the application.")
 Start = b'\x16'
-# + struct.pack("B", 0)
+
 i = 0
 while 1:
-    time.sleep(0.00001)
     x = ser.read(16)
-    # x = ser.read(88)
     ser.write(Start)
 
     if i != 0:
@@ -33,7 +31,6 @@ while 1:
         print(f"atr_signal: {atr_sig}")
         print(f"vent_sig: {vent_sig}")
 
-    # value3 = struct.unpack("b", x[0:8])[0]
     i = 1
     # print(x)
 
